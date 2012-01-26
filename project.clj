499eab8cff32eacf812e4ddde5c812c04e1bb1ee
Alex Collins <alextcollins@gmail.com>
2012-01-26 18:16:15 +0000

    add lein

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clojure-playground "1.0.0-SNAPSHOT"
+  :description "A sandbox for learning clojure; holds various solutions to common problems"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/clojure_playground/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-playground.test.core
+  (:use [clojure-playground.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
