0227e8958b9452d582137dd04acefa1cd658ef8b
Alex Collins <alextcollins@gmail.com>
2012-01-31 15:07:13 +0000

    Add unit tests

--- test/clojure_playground/test/core.clj
+++ test/clojure_playground/test/core.clj
@@ -5,14 +5,17 @@
 
 (def LARGE_VECT (vec (range 20000)))
 
-(deftest test-swap1 
-  (is (= [1 3 2] (swap [1 2 3] 1 2)) "swap must return [1 3 2] when given input [1 2 3] and told to swap elements 1 and 2"))
+(def SMALL_VECT [1 2 3])
 
-(deftest test-swap2 
-  (is (= [3 2 1] (swap [1 2 3] 1 0)) "swap must return [3 2 1] when given input [1 2 3] and told to swap elements 2 and 0"))
+(deftest test-swap-small-input
+  (is (= [1 3 2] (swap SMALL_VECT 1 2)) "swap must return [1 3 2] when given input SMALL_VECT and told to swap elements 1 and 2")
+  (is (= [2 1 3] (swap SMALL_VECT 1 0)) "swap must return [2 1 3] when given input SMALL_VECT and told to swap elements 2 and 0")
+  (is (= [2 3 1] (swap (swap SMALL_VECT 0 1) 1 2)) "swap must return [2 3 1] after multiple (nested) calls: swap 0 and 1 then 1 and 2")
+  (is (= [3 2 1] (swap (swap (swap SMALL_VECT 0 1) 1 2) 0 1)) "swap must return [3 2 1] after multiple (nested) calls: swap 0 and 1 then 1 and 2 and finally 0 and 1"))
 
-(deftest test-swap3 
-  (is (= (assoc (assoc LARGE_VECT  0 1) 1 0) (swap LARGE_VECT 1 0)) "swap must return [1 0...19999] for call with LARGE_VECT and told to swap elements 1 and 0"))
-
-(deftest test-swap4 
+(deftest test-swap-large-input
+  (is (= (assoc (assoc LARGE_VECT  0 1) 1 0) (swap LARGE_VECT 1 0)) "swap must return [1 0...19999] for call with LARGE_VECT and told to swap elements 1 and 0")
   (is (= LARGE_VECT (swap LARGE_VECT 0 0)) "swap must return LARGE_VECT for call with LARGE_VECT and told to swap elements 0 and 0"))
+
+; (deftest test-random-permutation
+;   (is (= 5 (random-permutation 0 5))))
