9242b06ee9a87a6e47ae55c6c50dc61d6a40e64a
Alex Collins <alextcollins@gmail.com>
2012-01-31 15:05:37 +0000

    Add main class declaration

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojure-playground "1.0.0-SNAPSHOT"
   :description "A sandbox for learning clojure; holds various solutions to common problems"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :aot [clojure-playground.core]
+  :main clojure-playground.core)
