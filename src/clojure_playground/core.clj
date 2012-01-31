348165474c7e43114a5a72bf7840f3fb3c6a7ad0
Alex Collins <alextcollins@gmail.com>
2012-01-31 15:08:13 +0000

    Begin starting point for whole playground

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -1,1 +1,11 @@
-(ns clojure-playground.core)
+(ns clojure-playground.core
+  (:require [clojure-playground.perm.generate :as pg]
+            [clojure-playground.shuffle.brute :as sb])
+  (:gen-class))
+
+(defn -main
+  [a1 a2 a3]
+  (do
+    (println a1 a2 a3)))
+    ;(if (= "shuffle" a1) 
+    ;  (sb/shuf (rest a2 a3)))))
--- src/clojure_playground/perm/generate.clj
+++ src/clojure_playground/perm/generate.clj
@@ -9,23 +9,17 @@
 (defn swap
   "For the given vector, swap the values at elements x and y respectively and return"
   [v x y]
-  (assoc (assoc v x (get v y)) y (get v x)))
+  (assoc (assoc v x (v y)) y (v x)))
 
 (defn random-permutation
-  "Generate a random permutation of numbers based on a set defined by (rand i n)"
+  "Generate a random permutation of numbers based on a set defined by (random i n)"
   [i N]
   (dotimes
     [n N]
     (let 
       [random-num (random i N) ; get a random number between i and N
-       nums (vec (range random-num))] ; all the numbers up to random-num in a vector
-      (swap nums name (random i N))
+       nums (vec (range random-num)) ; all the numbers up to random-num in a vector
+       r-num (random i N)] ; a random number each element
+      (swap nums name r-num)
     )))
 
-(defn main []
-  ;(random-permutation (Integer/parseInt (first *command-line-args*)) (Integer/parseInt (second *command-line-args*))))
-  (do
-    (def v (swap [1 2 3] 1 2))
-    (println v))) ; should yield [1 3 2]
-
-(main)
